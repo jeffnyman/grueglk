389713ed566ca5fe47cd888436fe5ab8cd884ed8
Jeff Nyman <jeffnyman@gmail.com>
2024-10-15 05:24:15 -0500

    Add a no-op Glk loop.

--- src/grue.c
+++ src/grue.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "glk.h"
 
 void glk_main()
 {
   printf("Grue - connected to MinGlk\n");
+
+  event_t ev;
+
+  while (1)
+  {
+    glk_select(&ev);
+    switch (ev.type)
+    {
+    default:
+      /* do nothing */
+      break;
+    }
+  }
 }
